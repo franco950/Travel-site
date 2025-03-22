5550f6e420aa13f37deb6433a71d9d1993ab55c7
Danny <127716079+franco950@users.noreply.github.com>
2025-03-22 14:36:07 +0300

    react error
    fixed react error caused by calling hooks after return statement, and added one more usestate and useeffect for filtering destinations

--- client/src/destinationpage.tsx
+++ client/src/destinationpage.tsx
@@ -1,25 +1,35 @@
-import React, { useState,useMemo } from "react";
+import { useEffect, useState } from "react";
 import {useDestinations,Destination,searchDestinations} from "./data/destinations";
 interface DestinationCardProps {
   destination: Destination;
 }
 
+
 function DestinationPage(){
+  
   const { destinations, loading, error } = useDestinations();
-  if (loading) return <p>Loading destinations...</p>;
-  if (error) return <p>{error}</p>
+  if (loading) console.log('Loading destinations...')
+  if (error) console.log({error})
+
   const options=["name","country","city","continent","price","duration"]
+  console.log("right b4 new states")
+  const [filteredDestinations, setFilteredDestinations] = useState<Destination[]>([]);
+  const [answer,setAnswer]=useState("")
   const [query,setQuery]=useState("name")
+  
+  console.log("right after new states")
   function choose(){
     console.log('choose function works')
-    setQuery((prevQuery)=>{let i=((options.indexOf(prevQuery)+1)%options.length);
+    setQuery((prevQuery)=>{let i=(options.indexOf(prevQuery)+1)%options.length;
     return options[i]||"name"})}
-  const parameter = `${query}query`; 
-  const [answer,setAnswer]=useState("")
-  const filteredDestinations = useMemo(() => {
-    if (!answer) return destinations;
-    return searchDestinations(destinations, { namequery:'paris' });
-  }, [destinations, parameter, answer]);
+
+    useEffect(() => {
+      if (destinations.length > 0) {
+        const parameter = `${query}query`;
+        setFilteredDestinations(searchDestinations(destinations, { [parameter]: answer }));
+      }
+    }, [destinations, query, answer]);
+  
   const Destinationcards=filteredDestinations.map((destination,index)=>{return <DestinationCard key={index} destination={destination}/>})
   return (
     <>
